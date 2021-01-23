86dda829f72cd86620df178dc9471f2f6c3d21a1
Lucas Gabriel <lucas.gato.03092001@gmail.com>
2021-01-22 23:57:43 -0300

    Início projeto Zup

---
+++ Gerador-Numeros/src/main/main.java
@@ -0,0 +1,19 @@
+package main;
+
+import java.util.Scanner;
+
+public class main {
+	
+	public static void main(String[] args) {
+		int num;
+		Scanner ler = new Scanner(System.in);
+
+		System.out.println("Digite um número aleatório: ");
+		num = ler.nextInt();
+		
+		
+		
+		
+		System.out.println(num);
+	}
+}
